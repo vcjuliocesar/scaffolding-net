4c2df539e5ba8ab266a2e73ec8904cc36496d9ce
Julio Cesar Valadez Castañeda <Julio.Valadez@talma.com.mx>
2024-04-25 15:57:23 -0600

    created microservice Auth

---
+++ Scaffolding.Services.Auth/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Scaffolding.Core.DTOs;
+using Scaffolding.Core.Interfaces;
+using Scaffolding.Core.Services;
+using System.Reflection;
+using System;
+
+namespace Scaffolding.Services.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CreateUserDTO model)
+        {
+            try
+            {
+                var user = await _authService.RegisterAsync(model);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            try {
+                Console.WriteLine(login);
+                var token = await _authService.LoginAsync(login);
+                return Ok(new { token });
+            }
+            catch (Exception ex) 
+            {
+                return Unauthorized(new { message = ex.Message });
+            }
+        }
+    }
+}
---
+++ Scaffolding.Services.Auth/Program.cs
@@ -0,0 +1,31 @@
+using Scaffolding.Core.Interfaces;
+using Scaffolding.Core.Services;
+using Scaffolding.Core.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
